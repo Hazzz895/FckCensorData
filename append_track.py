51a937dfbbf0d3364a66131e8e118ce4156e5129
Hazzz895 <kornilovily4@gmail.com>
2026-04-24 14:10:16 +0400

    быстрое включение треков через сокет (работй мой родненький чат джбт)

--- append_track.py
+++ append_track.py
@@ -1,19 +1,49 @@
 import json
 import os
+import threading
+import shutil
+import datetime
+import requests
 from dotenv import load_dotenv
 from git import Repo
+import yt_dlp
+from yandex_music import Client
+from websockets.sync.server import serve
 
 load_dotenv()
 yandex_music_token = os.getenv("API_TOKEN")
+sync_port = os.getenv("SYNC_PORT")
+supabase_secret_token = os.getenv("SUPABASE_SECRET_TOKEN")
 
-from yandex_music import Client
 client = (Client(yandex_music_token) if yandex_music_token else Client()).init()
 
-with open('list.json', 'r') as f:
+with open('list.json', 'r', encoding='utf-8') as f:
     data = json.loads(f.read())
 
-def start_appending(id, track_name = None):
+con = None
+def run_ws_server(port):
+    def handler(ws):
+        global con
+        con = ws
+        while True:
+            try:
+                ws.recv()
+            except:
+                break
+    with serve(handler, "127.0.0.1", port) as server:
+        server.serve_forever()
 
+def send_ws(msg):
+    if not con:
+        return
+    msg_str = json.dumps(msg)
+    con.send(msg_str)
+
+if sync_port:
+    ws_thread = threading.Thread(target=run_ws_server, args=(int(sync_port),), daemon=True)
+    ws_thread.start()
+
+def start_appending(id, track_name=None):
     if not track_name:
         print("Fetching track info...")
         track_info = client.tracks([id])[0]
@@ -23,15 +53,13 @@ def start_appending(id, track_name = None):
 
     repo = Repo('.')
     should_download = True
-    if (url):
+    if url:
         if not url.startswith('http'):
-            import shutil
             shutil.copy(url, f'tracks/{id}')
             print(f'File copied to tracks/{id}')
         else:
             should_download = input("Download track? (y/n): ").lower() == 'y'
             if should_download:
-                import yt_dlp
                 def download_sound(url):
                     ydl_opts = {
                         'format': 'bestaudio/best',
@@ -44,35 +72,36 @@ def start_appending(id, track_name = None):
 
     data['tracks'][id] = f'https://raw.githubusercontent.com/Hazzz895/FckCensorData/refs/heads/main/tracks/{id}' if should_download else url
 
-    with open('list.json', 'w') as f:
+    with open('list.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4)
         
     with open('README.md', 'a', encoding='utf-8') as f:
-        f.write(f'\n- [{track_name}](https://music.yandex.ru/track/{id})')
+        f.write(f'\n-[{track_name}](https://music.yandex.ru/track/{id})')
 
     repo.index.add(['list.json', f'tracks/{id}', "README.md"])
     repo.index.commit(f"add track «{track_name}»")
     print(f'Successfully added track {track_name}\n')
     
-supabase_secret_token = os.getenv("SUPABASE_SECRET_TOKEN")
 if supabase_secret_token:
-    import requests
-    response = requests.get("https://pzomqvgckpgkshxhpite.supabase.co/rest/v1/reported_tracks?select=*", headers = {
+    response = requests.get("https://pzomqvgckpgkshxhpite.supabase.co/rest/v1/reported_tracks?select=*", headers={
         "apikey": supabase_secret_token,
         "Authorization": f"Bearer {supabase_secret_token}",
         "Content-Type": "application/json"
-    });
+    })
     reports = response.json()
     
-    with open('rejected_tracks.dev.json', 'r') as f:
+    with open('rejected_tracks.dev.json', 'r', encoding='utf-8') as f:
         rejected_tracks = json.loads(f.read())
     
-    reports = [report for report in reports if str(report["track_id"]) not in data["tracks"] and not report["track_id"] in rejected_tracks]
+    reports = [report for report in reports if str(report["track_id"]) not in data["tracks"] and report["track_id"] not in rejected_tracks]
     
     for i, report in enumerate(reports):
         print(f'== {len(reports) - i} unreviewed tracks remaining ==')
-        import datetime
         id = report["track_id"]
+        
+        if sync_port:
+            send_ws({"id": id})
+                
         track_info = client.tracks([id])[0]
         track_name = f'{track_info.title} - {(", ".join(track_info.artistsName()))}'
         print(f'Track name: {track_name}')
@@ -80,13 +109,13 @@ if supabase_secret_token:
         skip = input(" - should append? ") == ""
         if skip:
             rejected_tracks.append(id)
-            with open('rejected_tracks.dev.json', 'w') as f:
+            with open('rejected_tracks.dev.json', 'w', encoding='utf-8') as f:
                 json.dump(rejected_tracks, f)
             print("Rejected.")
         else:
             start_appending(id, track_name)
 
 while True:
-    id = input("Yandex Music track ID or URL: ").split('/')[-1].split('?')[0]
+    user_input = input("Yandex Music track ID or URL: ")
+    id = user_input.split('/')[-1].split('?')[0]
     start_appending(id)
-    
